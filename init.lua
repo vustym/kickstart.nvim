6ca5d4fadbb13afb381e8728ad0a4f3dcedf9808
Victor Ustymchuk <victor.ustymchuk@stonebranch.com>
2023-10-13 16:16:47 +0300

    fix lsp

--- init.lua
+++ init.lua
@@ -458,7 +458,11 @@ require('which-key').register({
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  -- clangd = {},
+  clangd = {},
+  cmakelang = {},
+  cpptools = {},
+  cpplint = {},
+
   -- gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
